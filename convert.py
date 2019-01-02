6bbe2597cd9b1c030d8305f8e57f49d4a8608a50
btingle <43322227+btingle@users.noreply.github.com>
2019-01-02 06:53:55 -0800

    Add files via upload

--- convert.py
+++ convert.py
@@ -1,6 +1,51 @@
 import argparse, re, math
 from PIL import Image
 
+HEIGHT = None
+WIDTH = None
+
+# breaks down expression string into a number string
+def do_math(exp_string):
+	operators = ["+","-","*","/"]
+	exp_string = exp_string.replace("H", str(HEIGHT))
+	exp_string = exp_string.replace("W", str(WIDTH))
+	level = 0
+	maxlv = 0
+	number = True
+	if exp_string[0] == "(" and exp_string[-1] == ")":
+		exp_string = exp_string[1:-1]
+	for i in range(len(exp_string)):
+		level += 1 if exp_string[i] == "(" else 0
+		level -= 1 if exp_string[i] == ")" else 0
+		maxlv = level if level > maxlv else maxlv
+		if level == 0 and exp_string[i] in operators:
+			number = False
+			left = do_math(exp_string[:i])
+			right = do_math(exp_string[i+1:])
+			exp_string = left + exp_string[i] + right
+			break
+	op = None
+	oi = None
+	exp_string = exp_string.strip("()")
+	for i in range(len(exp_string)):
+		if exp_string[i] in operators:
+			op = exp_string[i]
+			oi = i
+			break
+	exp_string = exp_string.strip(op)
+	if not op:
+		return exp_string
+	L = int(exp_string[:oi])
+	R = int(exp_string[oi+1:])
+	if op == "+":
+		return str(L + R)
+	if op == "*":
+		return str(L * R)
+	if op == "/":
+		return str(L // R)
+	if op == "-":
+		return str(L - R)
+
 # scale : (%X, %Y)
 # pixels : (X, Y)
 # area : (A)
@@ -13,27 +58,27 @@ def get_mode(r_str):
 	if len(resize) == 1:
 		if "%" in resize[0]:
 			xmode = "scale"
-			xmod  = int(resize[0].strip("%")) / 100
+			xmod  = int(do_math(resize[0].strip("%"))) / 100
 			ymode = xmode
 			ymod  = xmod
 		else:
 			xmode = "area"
-			xmod  = int(resize[0])
+			xmod  = int(do_math(resize[0]))
 			ymode = xmode
 			ymod  = xmod
 	elif len(resize) == 2:
 		if "%" in resize[0]:
 			xmode = "scale"
-			xmod  = int(resize[0].strip("%")) / 100
+			xmod  = int(do_math(resize[0].strip("%"))) / 100
 		else:
 			xmode = "pixels"
-			xmod  = int(resize[0])
+			xmod  = int(do_math(resize[0]))
 		if "%" in resize[1]:
 			ymode = "scale"
-			ymod  = int(resize[1].strip("%")) / 100
+			ymod  = int(do_math(resize[1].strip("%"))) / 100
 		else:
 			ymode = "pixels"
-			ymod  = int(resize[1])
+			ymod  = int(do_math(resize[1]))
 	else:
 		print("Too many arguments!")
 	return xmode, xmod, ymode, ymod
@@ -48,31 +93,41 @@ def get_extension_type(filename):
 		return "JPEG"
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(prog="PROG", description="Resize and convert images", add_help=True)
+	parser = argparse.ArgumentParser(prog="PROG", 
+		description="Resize and convert images.\n"
+			   +"Numerical parameters can take the form of basic mathematical expressions, with single arithmetical operators, parentheses, and builtin variables H and W for Height and Width of input image.", add_help=True)
 	parser.add_argument("infile")
-	parser.add_argument("outfile")
-	parser.add_argument("-r", "--resize", metavar="WxH, N%, W%xH%, AREA", dest="r",
-			    help="Resize image, specify dimensions, percentage, or desired area. Pixels and percentage can be mixed, e.g: H%%xW.")
-	parser.add_argument("--preserve", action="store_true", default=False, help="Preserve image aspect ratio, pads image if this is not possible. Defaults to false.")
-	parser.add_argument("-p", metavar="WxH", default=False, help="Pad out image to specified size.")
-	parser.add_argument("-c", metavar="0xFFFFFF", default="0x000000", help="Sets padding color. Defaults to black")
+	parser.add_argument("outfile", nargs="?")
+	parser.add_argument("-r", "--resize", metavar="WxH, N%, W%xH%, AREA", dest="r", help="Resize image, specify dimensions, percentage, or desired area. Pixels and percentage can be mixed, e.g: H%%xW.")
+	parser.add_argument("-s", "--preserve", action="store_true", default=False, help="Preserve image aspect ratio, pads image if this is not possible. Defaults to false.")
+	parser.add_argument("-p", "--pad", metavar="WxH", default=False, dest="p", help="Pad out image to specified size.")
+	parser.add_argument("-d", "--dimensions", action="store_true", default=False, dest="d", help="Print dimensions of image file")
+	parser.add_argument("-x", "--crop", metavar="X:Y", nargs=2, help="Specify top left and bottom right corners of the bounding box you want to crop to.", dest="x")
+	parser.add_argument("-c", "--color", metavar="0xFFFFFF", default="0x000000", help="Sets padding color. Defaults to black")
 	args = parser.parse_args()
-	if not (args.r or args.p):
-		print("Please select an image operation")
-		exit(0)
+
 	image = None
 	try:
 		image = Image.open(args.infile)
 	except Exception as e:
 		print(str(e))
 		exit(0)
+	if args.d:
+		print("Dimensions, X:" + str(image.size[0]) + ", Y:" + str(image.size[1]))
+		exit(0)
+	if not (args.r or args.p or args.x):
+		print("Please select an image operation")
+		exit(0)
 	ext = get_extension_type(args.outfile)
+	print("Starting dimensions, X:" + str(image.size[0])+ ", Y:" + str(image.size[1]))
+	WIDTH  = image.size[0]
+	HEIGHT = image.size[1]
 	if args.r:
 		xmode, xmod, ymode, ymod = None, None, None, None
 		try:
 			xmode, xmod, ymode, ymod = get_mode(args.r)
-		except:
-			print("Invalid dimensions syntax")
+		except Exception as e:
+			print("Invalid dimensions syntax!")
 			exit(0)
 		xdim = image.size[0]
 		ydim = image.size[1]
@@ -107,11 +162,18 @@ if __name__ == "__main__":
 			xdim  = int(xdim)
 			ydim  = int(ydim)
 			image = image.resize((xdim, ydim), Image.ANTIALIAS)
-	if(args.p):
+	if args.p:
 		# creates a padding canvas to paste image into, pastes into the center no matter what, ignoring if edges get cropped
-		padx  = int(args.p.split("x")[0])
-		pady  = int(args.p.split("x")[1])
+		padx  = int(do_math(args.p.split("x")[0]))
+		pady  = int(do_math(args.p.split("x")[1]))
 		padim = Image.new("RGB", size=(padx, pady), color=int(args.c, 16))
 		padim.paste(image, box=(int((padx - xdim)/2), int((pady - ydim)/2)))
 		image = padim
+	if args.x:
+		left  = int(do_math(args.x[0].split(":")[0]))
+		upper = int(do_math(args.x[0].split(":")[1]))
+		right = int(do_math(args.x[1].split(":")[0]))
+		lower = int(do_math(args.x[1].split(":")[1]))
+		image = image.crop(box=(left, upper, right, lower))
+	print("Changed dimensions, X:" + str(image.size[0]) + ", Y:" + str(image.size[1]))
 	image.save(args.outfile, format=ext)
